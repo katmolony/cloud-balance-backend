6abbd7f617b942822aab9e10b75f8a5e54a82f9c
Kate Molony <katemolony96@gmail.com>
2025-03-22 15:01:16 +0000

    Allow SSL in prod and add cloudwatch logging.

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,20 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-if (!process.env.DATABASE_URL) {
-  console.log("Using test DB connection:", `postgres://${process.env.TEST_DB_USER}@${process.env.TEST_DB_HOST}/${process.env.TEST_DB_NAME}`);
-} else {
-  console.log("Using DATABASE_URL for production.");
-}
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || `postgres://${process.env.TEST_DB_USER}:${process.env.TEST_DB_PASSWORD}@${process.env.TEST_DB_HOST}:${process.env.TEST_DB_PORT}/${process.env.TEST_DB_NAME}`,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+  connectionString: connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
+
+// Debug logs to verify in CloudWatch
+console.log("DATABASE_URL:", connectionString);
+console.log("SSL Mode:", isProduction ? "ENABLED" : "DISABLED");
+console.log("DB Pool Config:", {
+  connectionString: connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
